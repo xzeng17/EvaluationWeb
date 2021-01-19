c3a38a869f7e84ba8c8fc21f1d6f0d0cd476bea9
Xuankun <xzeng17@illinois.edu>
2021-01-18 21:46:19 -0600

    update on forms

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # EvaluationWeb
+
+# As a teaching assistant for MCB151 at UIUC, I designed a simple evaluation form for students to submit 
+# anonymous evaluation form during semester. 
+
+# This static website collect and post students' forms to my backend server and saved at Mongodb atlas.
---
+++ main.js
@@ -0,0 +1,98 @@
+(function() {
+    function init() {
+        console.log("initialized");
+        document.querySelector('#submit-btn').addEventListener('click', submission);
+    }
+
+    function submission() {
+        console.log("submit clicked");
+        showSubmitMessage("Processing!!");
+
+        var section = document.querySelector('#section').value;
+        var positiveFB = document.querySelector('#positiveFB').value;
+        var negativeFB = document.querySelector('#negativeFB').value;
+
+        if (section === "") {
+            showSubmitMessage("Please select your section!");
+            return;
+        }
+        disableSubmitBtn('submit-btn');
+        var url = "https://evaluationform.herokuapp.com/submitevaluation";
+        var req = JSON.stringify({
+            section: section,
+            positiveFB: positiveFB,
+            negativeFB: negativeFB,
+        });
+
+        ajax("POST", url, req, 
+            function(res) {
+                var result = JSON.parse(res);
+                console.log(result);
+                if (result.status === 'OK') {
+                    showSubmitMessage('Submission completed successfully!');
+                } else {
+                    enableSubmitBtn('submit-btn');
+                    showSubmitMessage('Something wrong with backend server, please contact Xuankun.');
+                }
+            },
+
+            function() {
+                enableSubmitBtn('submit-btn');
+                showSubmitMessage('Request cannot be sent, please contact Xuankun.');
+            }
+        )  
+    }
+
+
+    function showSubmitMessage(msg) {
+        var itemList = document.querySelector('#status-message');
+        itemList.innerHTML = msg;
+    }
+
+    function enableSubmitBtn(btnid) {
+        document.getElementById(btnid).disabled = false;
+    }
+
+    function disableSubmitBtn(btnid) {
+        document.getElementById(btnid).disabled = true;
+    }
+
+    /* AJAX helper
+    *   @param method - GET|POST|PUT|DELETE
+    *   @param url - API end point
+    *   @param data - request payload data
+    *   @param successCallback - Successful callback function
+    *   @param errorCallback - Error callback function
+    */
+    function ajax(method, url, data, successCallback, errorCallback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url, true);
+        // xhr.setRequestHeader("X-Auth-Token","api-key 1234567890");
+        // xhr.setRequestHeader('Access-Control-Allow-Origin','*');
+        // xhr.setRequestHeader('Access-Control-Allow-Methods', method);
+        // xhr.setRequestHeader('Access-Control-Allow-Headers','X-Auth-Token,Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+        // xhr.send();
+        console.log(xhr);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                successCallback(xhr.responseText);
+            } else {
+                errorCallback();
+            }
+        };
+
+        xhr.onerror = function() {
+            console.error("The request couldn't be completed.");
+            errorCallback();
+        };
+
+        if (data === null) {
+            xhr.send();
+        } else {
+            xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+            xhr.send(data);
+        }
+    }
+
+    init();
+})();
