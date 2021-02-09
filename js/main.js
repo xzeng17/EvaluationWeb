be97e90f262a459d6897a0303634694e48171244
Xuankun <xzeng17@illinois.edu>
2021-02-08 21:09:46 -0600

    overall structure

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const coursedescriptionBTN = document.getElementById("course-description");
+const labnotesBTN = document.getElementById("lab-notes");
+const zoomlinksBTN = document.getElementById("zoom-links");
+const feedbackBTN = document.getElementById("feedback");
+
+coursedescriptionBTN.addEventListener("click", ()=>loadTemplate("templateCourseDescription"));
+labnotesBTN.addEventListener("click", ()=>loadTemplate("templateTAnotes"));
+zoomlinksBTN.addEventListener("click", ()=>loadTemplate("templateZoomLinks"));
+feedbackBTN.addEventListener("click", ()=>loadTemplate("templateEvaluation"));
+
+
+
+function loadTemplate(templateString) {
+    const templateContainer = document.getElementById("templates");
+    templateContainer.innerHTML = "";
+    const template = document.getElementById(templateString);
+    const clone = template.content.cloneNode(true);
+    templateContainer.appendChild(clone);
+}
+
+// init
+loadTemplate("templateCourseDescription");
